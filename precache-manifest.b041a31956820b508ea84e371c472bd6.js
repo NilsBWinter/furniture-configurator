392cedff90169b7a0d5d7826e84e85e59db9d35a
Nils Winter <nils@curv-labs.de>
2021-03-12 16:28:08 +0100

    deploy

---
+++ precache-manifest.b041a31956820b508ea84e371c472bd6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a56a8ad7beb2be6a54",
+    "url": "/furniture-configurator/css/app.f5f1fb2b.css"
+  },
+  {
+    "revision": "63f67a9603a576d435b8",
+    "url": "/furniture-configurator/css/calculator.9b4ef48c.css"
+  },
+  {
+    "revision": "043411a74da2752ec7c3",
+    "url": "/furniture-configurator/css/chunk-vendors.b32eef8c.css"
+  },
+  {
+    "revision": "888d48f5629b3627f6d2f491efe4ffae",
+    "url": "/furniture-configurator/index.html"
+  },
+  {
+    "revision": "baf896427ba5ac2b92d4",
+    "url": "/furniture-configurator/js/about.c9c5e935.js"
+  },
+  {
+    "revision": "25a56a8ad7beb2be6a54",
+    "url": "/furniture-configurator/js/app.fe106dd9.js"
+  },
+  {
+    "revision": "63f67a9603a576d435b8",
+    "url": "/furniture-configurator/js/calculator.06057347.js"
+  },
+  {
+    "revision": "043411a74da2752ec7c3",
+    "url": "/furniture-configurator/js/chunk-vendors.0d167227.js"
+  },
+  {
+    "revision": "e77faa23353f485714a91d945363ec59",
+    "url": "/furniture-configurator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/furniture-configurator/robots.txt"
+  }
+]);
